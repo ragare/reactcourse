68d64a834571f7d192191836ff09350c64966e54
Rafael Garcia <dev.ragare@gmail.com>
2019-08-20 18:54:49 +0200

    Before install Babel

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,6 +1,10 @@
 console.log("React app is runnig!");
 
 // JSX
-var template = <p>This is JSX from app.js</p>;
+// var template = <p>This is JSX from app.js</p>;
+var template = React.createElement("p", {
+    id: "mp"
+}, "This is JSX from app.js");
+var appRoot = document.getElementById('app')
 
-ReactDOM.render(template, "app");
+ReactDOM.render(template, appRoot);
