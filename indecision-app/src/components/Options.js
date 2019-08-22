180e3f2f9c397d9201a3bb6035ab9de923b71592
Rafael Garcia <dev.ragare@gmail.com>
2019-08-22 21:58:56 +0200

    before styling

--- indecision-app/src/components/Options.js
+++ indecision-app/src/components/Options.js
@@ -1,24 +1,22 @@
 import React from 'react'
 import Option from './Option'
 
-const Options = (props) => {
-    return (
-        <div>
-            <button onClick={props.handleDeleteOptions}>Remove all</button>
-            {
-                props.options.length == 0 && <p>No options available</p>
-            }
-            {
-                props.options.map((option) => (
-                    <Option
-                        key={option}
-                        optionText={option}
-                        handelDeleteOption={props.handelDeleteOption}
-                    />
-                ))
-            }
-        </div>
-    );
-}
+const Options = (props) => (
+    <div>
+        <button onClick={props.handleDeleteOptions}>Remove all</button>
+        {
+            props.options.length == 0 && <p>No options available</p>
+        }
+        {
+            props.options.map((option) => (
+                <Option
+                    key={option}
+                    optionText={option}
+                    handelDeleteOption={props.handelDeleteOption}
+                />
+            ))
+        }
+    </div>
+)
 
 export default Options
